aa52c4f76c25e0244ad36033e5de4232ebd8d245
John Paul Kiser <johnpaulkiser@gmail.com>
2019-02-27 14:38:03 -0500

    added LOSER

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ can = document.getElementById("canvas");
 ctx = can.getContext("2d");
 ctx.fillStyle = "blue";
 ctx.fillRect(0,0, 50, 50);
-
+var jp = "Loser"
 setInterval(update, 50);
 
 function update(){
-    console.log("jp");
+    console.log(jp);
 }
